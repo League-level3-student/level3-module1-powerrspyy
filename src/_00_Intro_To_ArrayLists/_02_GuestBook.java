358470709e4f84ee7f048b3aa1a9b437acf15d72
powerspy <powerspy999@gmail.com>
2025-02-27 19:59:37 -0800

    started GuestBook

--- src/_00_Intro_To_ArrayLists/_02_GuestBook.java
+++ src/_00_Intro_To_ArrayLists/_02_GuestBook.java
@@ -1,6 +1,13 @@
 package _00_Intro_To_ArrayLists;
 
-public class _02_GuestBook {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class _02_GuestBook implements ActionListener {
     /*
      * Create a GUI with two buttons. One button reads "Add Name" and the other
      * button reads "View Names". When the add name button is clicked, display
@@ -13,5 +20,44 @@ public class _02_GuestBook {
      * Guest #3: Greg Ganders
      * Guest #4: Donny Doners
      */
+	JPanel panel = new JPanel();
+	JFrame frame = new JFrame("GuestBook");
+	JButton addName = new JButton("Add Name");
+	JButton viewNames = new JButton("View Names");
+	
+	public static void main(String[] args) {
+		_02_GuestBook guestBook = new _02_GuestBook();
+		guestBook.buildGUI();
+	}
+
+	private void buildGUI() {
+		// TODO Auto-generated method stub
+		panel.add(addName);
+		panel.add(viewNames);
+		
+		frame.add(panel);
+		
+		addName.addActionListener(this);
+		
+		viewNames.addActionListener(this);
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		frame.pack();
+		
+		frame.setVisible(true);
+		
+	
+	}
+	
+	
+	@Override
+	public  void actionPerformed(ActionEvent e) {
+		
+		if(e.getSource() == addName) {
+			
+		}
+	}
+	
 
 }
